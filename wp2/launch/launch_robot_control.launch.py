79a6cc538b69eb7e5883589fd8703a1f163bfa0e
Alexander Lupatsiy <alexander@lupatsiy.de>
2026-03-09 12:07:23 +0100

    added template for wp2

---
+++ wp2/launch/launch_robot_control.launch.py
@@ -0,0 +1,23 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, Command, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+
+    # Launch the robot_control node from workpackage_2 package
+    workpackage_2_node = Node(
+        package='workpackage_2',
+        executable='robot_control',
+        name='robot_control',
+        output='screen'
+    )
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            description='Use simulation (Gazebo) clock if true'),
+        workpackage_2_node
+    ])
---
+++ wp2/src/robot_control.cpp
@@ -0,0 +1,9 @@
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/joint_state.hpp>
+#include <surros_lib/kinematics.h>
+#include <surros_lib/surros_interface.h>
+
+int main(int argc, char **argv)
+{
+
+}
