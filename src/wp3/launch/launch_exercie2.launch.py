bafcf4e8f7fe06e8ae1106bb0d9c1fd4bf92e543
Alexander Lupatsiy <alexander@lupatsiy.de>
2026-03-09 22:09:33 +0100

    added launch file for exercie2 wp3

---
+++ src/wp3/launch/launch_exercie2.launch.py
@@ -0,0 +1,23 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, Command, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+
+    # Launch the robot_control node from workpackage_3 package
+    workpackage_3_node = Node(
+        package='workpackage_3',
+        executable='exercise2',
+        name='exercise2',
+        output='screen'
+    )
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            description='Use simulation (Gazebo) clock if true'),
+        workpackage_3_node
+    ])
